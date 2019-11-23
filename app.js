0ce9ff92ad1bd8abce431db54d2475a54594286f
Anatole Lucet <35486736+AnatoleLucet@users.noreply.github.com>
2019-11-23 18:11:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
+app.get("/t", (req, res) => res.send("Hello World from t!"));
+
 var server = app.listen(3000, () => {
   console.log("Listening on port " + server.address().port + "...");
 });
